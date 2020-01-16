1b826a8f75dcc659f2c25b9fe5ee26c7d4af0050
Pascal Gläßer <otakupasi@users.noreply.github.com>
2020-01-16 18:15:50 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
+git -v >/dev/null 2>&1
+GIT=$?
 
 gcc -v >/dev/null 2>&1
 GCC=$?
 
-if [[ ${GCC} -nw 0 ]]
+if [[ ${GCC} -ne 0 ]]
   then echo -e "\e[31mYou need to install gcc first\e[0m\n"
 fi
 
+if [[ ${GIT} -ne 0 ]]
+  then echo -e "\e[31mYou need to install git first\e[0m\n"
+fi
+
+USER=$(git config user.name)
+
 gcc -o Urlaubsplanner create.c -lm
 ./Urlaubsplanner
+
+echo "Now uploading your File to Github\n"
+git checkout -b $USER
+git add .
+git commit -m "AUTO-COMMIT: Adding file from Urlaubsplanner"
+git push origin
+echo "Finished \n\n"
